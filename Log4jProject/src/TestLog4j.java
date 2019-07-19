5dcf2ef6cdd9d61232a930d1349dd3f0f216b541
5003742 <5003742@INGUEUCWS000639.camkmg-bangalore.com>
2019-07-19 12:09:56 +0530

    Testing Apache Log4j - Console

---
+++ Log4jProject/src/TestLog4j.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+
+public class TestLog4j {
+	
+	
+	static Logger Log = Logger.getLogger(TestLog4j.class);
+
+	public static void main(String[] args) {
+		
+	Log.debug("This is the debug log");
+	Log.info("This is the info log");
+	Log.error("Error is");
+
+	}
+
+}
